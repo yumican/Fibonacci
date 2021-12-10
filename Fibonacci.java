f1f6cee97e8bfaccccd4ad8bd400b979546c6238
yumican <89607931+yumican@users.noreply.github.com>
2021-12-10 14:31:54 +0300

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+
+public class Fibonacci {
+    static int fib(int a){
+        if (a==1 ||a==2)
+return 1;
+        return fib(a - 1 ) + fib(a - 2 );
+
+    }
+public static void main(String[]args){
+    System.out.println(fib(6));
+}
+}
+
